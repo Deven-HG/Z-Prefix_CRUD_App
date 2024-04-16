fcbe076af19283fd35478c327bf0c804341b0be1
Deven <hudde13@gmail.com>
2024-04-16 08:32:27 -0600

    init commit

---
+++ client/inventory_management/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <body className="App-header">
+        <p>
+          this is where i would put my inventory.... IF I HAD ONE
+        </p>
+      </body>
+    </div>
+  );
+}
+
+export default App;
